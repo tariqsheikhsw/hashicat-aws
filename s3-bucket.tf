e14c1ec809a396e3082bbdc5cd53125a81f04e99
Tariq Sheikh <tariqsheikhsw@gmail.com>
2022-07-21 11:39:26 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,43 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "aws-s3-bk-tariq-786-sheikh"
+  prefix = "tarioqsh"
+  tags = "bucket"
+}
 
-  bucket = "tariqsheikh-s3-bucket-786"
-  bucket_prefix = "tariqsheikh"
-  acl    = "private"
+resource "aws_s3_bucket_website_configuration" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
 
-  versioning = {
-    enabled = true
+  index_document {
+    suffix = "index.html"
   }
 
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.s3_bucket.arn,
+          "${aws_s3_bucket.s3_bucket.arn}/*",
+        ]
+      },
+    ]
+  })
 }
